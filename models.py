25c2f2ed222e365674a2e69a831426ed918667d6
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-07 15:20:52 -0300

    Added request_name

---
+++ models.py
@@ -0,0 +1,26 @@
+from openerp import models, fields, api, _
+from openerp.osv import osv
+from openerp.exceptions import except_orm, ValidationError
+from StringIO import StringIO
+import urllib2, httplib, urlparse, gzip, requests, json
+import openerp.addons.decimal_precision as dp
+import logging
+import datetime
+from openerp.fields import Date as newdate
+
+#Get the logger
+_logger = logging.getLogger(__name__)
+
+class purchase_order(models.Model):
+	_inherit = 'purchase.order'
+
+	@api.one
+	def _compute_request_name(self):
+		return_value = ''
+		for line in self.order_line:
+			if line.purchase_request_lines:
+				import pdb;pdb.set_trace()
+		self.request_name = return_value
+
+	request_name = fields.Char(string='Requisicion',compute=_compute_request_name)
+
