78e22f85e79b874c180776e65e8b22dc50ce363e
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-07 12:34:09 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name": "AS Customizations",
+    "author": "Eficent",
+    "website": "www.eficent.com",
+    "category": "Generic Modules/Projects & Services",
+    "depends": ["project","analytic","account","purchase"],
+    "init_xml": [],
+    "update_xml": [
+	'report_purchasequotation.xml'
+    ],
+    'demo_xml': [
+    ],
+    'test':[
+    ],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
