120a130f003e0844ff13c83bbe02dbf8369d26aa
morrillo <gustavo.orrillo@moldeo.coop>
2017-01-02 16:26:07 -0300

    Added tipo_entrega

--- models.py
+++ models.py
@@ -193,7 +193,7 @@ class purchase_order(models.Model):
 	approver_id = fields.Many2one('res.users',string='Approver')
 	nro_remito = fields.Char('Nro.Remito',compute=_compute_nro_remito)
 	fecha_recepcion = fields.Char('Fecha Recepcion',compute=_compute_fecha_recepcion)
-	tipo_entrega = fields.Selection(selection=[('propio','Entrega en depósito &&Compañía'),('proveedor','Retiramos de Depósito del Proveedor'),('valle_soleado','Valle Soleado')],\
+	tipo_entrega = fields.Selection(selection=[('propio','Entrega en depósito &&Compañía'),('proveedor','Retiramos de Depósito del Proveedor'),('valle_soleado','Entrega en Valle Soleado')],\
 			string='Tipo de Entrega')
 	purchase_notes = fields.Text('Notas de compra')
 	user_deliver_to = fields.Many2one('res.users',string='Entregar a')
